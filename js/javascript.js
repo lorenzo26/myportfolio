b084e66f4a0ef3112b378e87d289e0d316270ccf
lorenzo26 <benedict.bueno26@gmail.com>
2017-08-20 22:50:40 +0800

    myportfolio v.2

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,9 @@
-$(function(){
-$('.HSshowit').click(function(e) {
-  var condition = $(this).attr("rev");
-  if(condition == "more"){
-    $(this).html("Show Less");
-    $(this).attr("rev","less");
-  }
-  else{
-    $(this).html("Show More");
-    $(this).attr("rev","more");
-  }
-});
-});
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
